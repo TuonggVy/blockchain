f131a53de8fa4f27bff6993b6f55391244878682
TuonggVy <hongoctuongvy261104@gmail.com>
2025-02-12 02:42:34 +0700

    add mainlayout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,18 +2,17 @@ import ContentBedrock from "./components/ContentBedrock/ContentBedrock";
 import ContentBitcoin from "./components/ContentBitcoin/ContentBitcoin";
 import ContentPowering from "./components/ContentPowering/ContentPowering";
 import LayerInfo from "./components/LayerInfo/LayerInfo";
-import NavBar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/Footer";
+import MainLayout from "../../layouts/MainLayout";
 
 function Home() {
   return (
     <>
-      <NavBar />
+      <MainLayout >
       <ContentBitcoin />
       <ContentPowering />
       <ContentBedrock />
       <LayerInfo />
-      <Footer />
+      </MainLayout>
     </>
   );
 }
