bb3c6eb4010f598453fcd8d15c9f321fd3592339
TuonggVy <hongoctuongvy261104@gmail.com>
2025-03-29 01:54:26 +0700

    archive

--- src/pages/Blog/BlogContent/AllContent/Archive/Archive.jsx
+++ src/pages/Blog/BlogContent/AllContent/Archive/Archive.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Archive.scss";
-import { BookOutlined, MessageOutlined, SearchOutlined } from "@ant-design/icons";
+import {
+  BookOutlined,
+  MessageOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
 
 const Archive = () => {
   const posts = [
@@ -29,113 +33,130 @@ const Archive = () => {
 
   return (
     <div className="archive-container">
-      <div className="posts-section">
-        {posts.map((post) => (
-          <div key={post.id} className="post">
-            <div className="post-header">
-              <img
-                src="https://cdn-images-1.medium.com/fit/c/72/72/1*h4_yDoHr9N6E2Ex90XYjxw.png"
-                alt="Logo"
-                className="post-logo"
-              />
-              <div className="post-info">
-                <span>LayerEdge</span>
-                <span>
-                  {post.date} • {post.readTime}
+      <div className="col-1">
+        <div className="archive-header">
+          <h2 className="archive-title">Archive of stories published by LayerEdge</h2>
+          <div className="archive-filters">
+            <span>All</span>
+            <span>{">"}</span>
+            <span className="filter-year">2024</span>
+            <span className="filter-year">2025</span>
+            <select className="sort-dropdown">
+              <option>Sort by most read</option>
+              <option>Sort by latest</option>
+            </select>
+          </div>
+        </div>
+        <div className="posts-section">
+          {posts.map((post) => (
+            <div key={post.id} className="post">
+              <div className="post-header">
+                <img
+                  src="https://cdn-images-1.medium.com/fit/c/72/72/1*h4_yDoHr9N6E2Ex90XYjxw.png"
+                  alt="Logo"
+                  className="post-logo"
+                />
+                <div className="post-info">
+                  <span>LayerEdge</span>
+                  <span>
+                    {post.date} • {post.readTime}
+                  </span>
+                </div>
+              </div>
+              <img src={post.image} alt="Post" className="post-image" />
+              <h2 className="post-title">{post.title}</h2>
+              <p className="read-more">Read more...</p>
+              <div className="post-actions">
+                <span className="icon-post-1">
+                  <SearchOutlined />
+                  <span>{post.likes}</span>
                 </span>
+                <span className="icon-post-2">
+                  <MessageOutlined />
+                  <span> {post.responses} responses</span>
+                </span>
+                <BookOutlined className="bookmark-icon" />
               </div>
             </div>
-            <img src={post.image} alt="Post" className="post-image" />
-            <h2 className="post-title">{post.title}</h2>
-            <p className="read-more">Read more...</p>
-            <div className="post-actions">
-              <span className="icon-post-1">
-                <SearchOutlined />
-                <span>{post.likes}</span>
-              </span>
-              <span className="icon-post-2">
-                <MessageOutlined />
-                <span> {post.responses} responses</span>
-              </span>
-              <BookOutlined className="bookmark-icon" />
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
 
       {/* Sidebar */}
-      <div className="sidebar-section">
-        <div className="archive-about">
-          <h3>About</h3>
-          <span className="about-name">LayerEdge</span>
-          <p className="about-description">
-            We are building Bitcoin-backed Internet using trust-minimized
-            verification & proof aggregation for all layers & apps.
-          </p>
-          <p className="more-info">More information</p>
-        </div>
+      <div className="col-2">
+        <div className="sidebar-section">
+          <div className="archive-about">
+            <h3>About</h3>
+            <span className="about-name">LayerEdge</span>
+            <p className="about-description">
+              We are building Bitcoin-backed Internet using trust-minimized
+              verification & proof aggregation for all layers & apps.
+            </p>
+            <p className="more-info">More information</p>
+          </div>
 
-        <div className="tags">
-          <h4>Tags</h4>
-          <ul>
-            <li>Layer 2</li>
-            <li>Bitcoin</li>
-            <li>Blockchain</li>
-            <li>Bitcoin</li>
-            <li>Zero Knowledge Proofs</li>
-            <li>Zkp</li>
-            <li>Boundless</li>
-            <li>Layeredge</li>
-            <li>Bitvm</li>
-            <li>Zero Knowledge</li>
-          </ul>
-        </div>
+          <div className="tags">
+            <h4>Tags</h4>
+            <ul>
+              <li>Layer 2</li>
+              <li>Bitcoin</li>
+              <li>Blockchain</li>
+              <li>Bitcoin</li>
+              <li>Zero Knowledge Proofs</li>
+              <li>Zkp</li>
+              <li>Boundless</li>
+              <li>Layeredge</li>
+              <li>Bitvm</li>
+              <li>Zero Knowledge</li>
+            </ul>
+          </div>
 
-        <div className="archive-editors">
-          <h4>Editors</h4>
-          <div className="archive-editor">
-            <img
-              src="https://cdn-images-1.medium.com/fit/c/120/120/1*h4_yDoHr9N6E2Ex90XYjxw.png"
-              alt="LayerEdge Logo"
-              className="editor-avatar"
-            />
-            <span>LayerEdge</span>
+          <div className="archive-editors">
+            <h4>Editors</h4>
+            <div className="archive-editor">
+              <img
+                src="https://cdn-images-1.medium.com/fit/c/120/120/1*h4_yDoHr9N6E2Ex90XYjxw.png"
+                alt="LayerEdge Logo"
+                className="editor-avatar"
+              />
+              <span>LayerEdge</span>
+            </div>
+            <div className="archive-editor">
+              <img
+                src="https://cdn-images-1.medium.com/fit/c/120/120/1*fvYHggwYOv4wFi7lcr9XMw.png"
+                alt="Ayush Saha"
+                className="editor-avatar"
+              />
+              <span>Ayush Saha</span>
+            </div>
+            <div className="archive-editor">
+              <img
+                src="https://cdn-images-1.medium.com/fit/c/120/120/0*ee7DbAnYqJzYWjiv"
+                alt="Ayush Gupta"
+                className="editor-avatar"
+              />
+              <span>Ayush Gupta</span>
+            </div>
+            <div className="archive-editor">
+              <img
+                src="https://cdn-images-1.medium.com/fit/c/120/120/1*n_ThvqmAnY6y65lxYs1Jcg.jpeg"
+                alt="Ayush Gupta"
+                className="editor-avatar"
+              />
+              <span>Rishsane</span>
+            </div>
           </div>
-          <div className="archive-editor">
-            <img
-              src="https://cdn-images-1.medium.com/fit/c/120/120/1*fvYHggwYOv4wFi7lcr9XMw.png"
-              alt="Ayush Saha"
-              className="editor-avatar"
-            />
-            <span>Ayush Saha</span>
-          </div>
-          <div className="archive-editor">
-            <img
-              src="https://cdn-images-1.medium.com/fit/c/120/120/0*ee7DbAnYqJzYWjiv"
-              alt="Ayush Gupta"
-              className="editor-avatar"
-            />
-            <span>Ayush Gupta</span>
-          </div>
-          <div className="archive-editor">
-            <img
-              src="https://cdn-images-1.medium.com/fit/c/120/120/1*n_ThvqmAnY6y65lxYs1Jcg.jpeg"
-              alt="Ayush Gupta"
-              className="editor-avatar"
-            />
-            <span>Rishsane</span>
-          </div>
-        </div>
 
-        <div className="archive-writers">
-          <h4>Writers</h4>
-          <div className="archive-writer">
-            <img
-              src="https://cdn-images-1.medium.com/fit/c/120/120/1*fvYHggwYOv4wFi7lcr9XMw.png"
-              alt="LayerEdge Logo"
-              className="writer-avatar"
-            />
-            <span>Ayush Saha</span>
+          <div className="archive-writers">
+            <h4>Writers</h4>
+            <div className="archive-writer">
+              <img
+                src="https://cdn-images-1.medium.com/fit/c/120/120/1*fvYHggwYOv4wFi7lcr9XMw.png"
+                alt="LayerEdge Logo"
+                className="writer-avatar"
+              />
+              <span>Ayush Saha</span>
+            </div>
           </div>
         </div>
       </div>
